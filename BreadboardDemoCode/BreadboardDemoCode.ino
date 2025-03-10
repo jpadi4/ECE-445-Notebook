57d854fa709522999c8f3376d8c2ab4660d8cdd5
Li Padilla <jpadi4@illinois.edu>
2025-03-10 14:28:14 -0500

    Push demo code

--- BreadboardDemoCode/BreadboardDemoCode.ino
+++ BreadboardDemoCode/BreadboardDemoCode.ino
@@ -1,5 +1,5 @@
-#define LED_PIN PIN_PB2   // Pin for LED output
-#define STRAIN_GAUGE_0 A2 // specifies using PB3 as an analog input pin
+#define LED_PIN PIN_PB1   // Pin for LED output
+#define STRAIN_GAUGE_0 A1 // specifies using PB3 as an analog input pin
 
 
 void setup() {
@@ -8,10 +8,10 @@ void setup() {
 
 void loop() {
     // read analog val (0-1023)
-    int analogValue = analogRead(STRAIN_GAUGE_0)
+    int analogValue = analogRead(STRAIN_GAUGE_0);
 
     // convert ADC val to voltage; 
-    float VCC = 3.3
+    float VCC = 3.3;
     float voltage = (analogValue * VCC) / 1023.0;
 
     // if the voltage from the strain gauge > 1.1V

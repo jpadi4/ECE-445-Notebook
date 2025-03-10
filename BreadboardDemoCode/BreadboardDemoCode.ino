c5e4a1d98f9c86810458f2ecf5a5b1a84a3b08ad
Li Padilla <jpadi4@illinois.edu>
2025-03-10 11:48:22 -0500

    Push Breadboard Demo .ino code file

---
+++ BreadboardDemoCode/BreadboardDemoCode.ino
@@ -0,0 +1,23 @@
+#define LED_PIN PIN_PB2   // Pin for LED output
+#define STRAIN_GAUGE_0 A1 // specifies using PB3 as an analog input pin
+
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+}
+
+void loop() {
+    // read analog val (0-1023)
+    int analogValue = analogRead(STRAIN_GAUGE_0)
+
+    // convert ADC val to voltage; 
+    float VCC = 3.3
+    float voltage = (analogValue * VCC) / 1023.0;
+
+    // if the voltage from the strain gauge > 1.1V
+    if (voltage > 1.1) {  
+      digitalWrite(LED_PIN, HIGH); // Turn on LED
+    } else {
+      digitalWrite(LED_PIN, LOW);  // Turn off LED
+    }
+}
